697d2e0f507762f6680ff2ca64f732fbf570416e
sakumatakumi <goose.326.hose.bibi@gmail.com>
2025-04-01 01:24:46 +0900

    エンティティの実装

--- src/main/java/com/example/demo/entity/Category.java
+++ src/main/java/com/example/demo/entity/Category.java
@@ -1,5 +1,36 @@
 package com.example.demo.entity;
 
-public class Category {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+//このクラスが JPA（Java Persistence API）のエンティティであることを示す
+@Entity
+//エンティティがマッピングされるデータベースのテーブル名を指定
+//テーブル名のeとsの間にスペースが入ってしまった。
+@Table(name = "categorie s")
+public class Category {
+	//エンティティの主キー（一意な識別子）となるフィールドを指定
+	@Id
+	//主キーの生成方法を指定
+	//GenerationType.IDENTITY は、データベースの自動インクリメント機能を使用して主キーを生成する
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id; //カテゴリーID
+
+	private String name; //カテゴリー名
+
+	//ゲッター
+	//エンティティのフィールドの値を取得するために使用
+	public Integer getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
 }
+
+//エンティティとは、RPGで考えるデータベースに保存されるデータの設計図みたいなもの
+//これらの「モノ」や「キャラクター」がどのような情報を持っているかを定義
