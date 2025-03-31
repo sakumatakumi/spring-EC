4d560c95f1b3aa72b1d41bd8c1338d98a134da3b
sakumatakumi <goose.326.hose.bibi@gmail.com>
2025-04-01 02:02:06 +0900

    リポジトリの実装

--- src/main/java/com/example/demo/repository/CategoryRepository.java
+++ src/main/java/com/example/demo/repository/CategoryRepository.java
@@ -1,5 +1,9 @@
 package com.example.demo.repository;
 
-public class CategoryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
 }
--- src/main/java/com/example/demo/repository/ItemRepository.java
+++ src/main/java/com/example/demo/repository/ItemRepository.java
@@ -1,5 +1,25 @@
 package com.example.demo.repository;
 
-public class ItemRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.Item;
+
+//ItemRepository インターフェースは、JpaRepository インターフェースを継承している
+//JpaRepository<Item, Integer> は、Item エンティティを操作し、
+//主キーの型が Integer であることを指定
+//JpaRepository を継承することで、save(), findById(), findAll(), delete() など
+//基本的な CRUD 操作を自動的に利用できる
+
+public interface ItemRepository extends JpaRepository<Item, Integer> {
+
+	//items テーブルから category_id が指定された値と一致する、
+	//Item エンティティのリストを取得
+	//SELECT * FROM items WHERE category_id = ?
+	//? には、categoryId の値が入る
+	//JpaRepository を継承してるため、
+	//save(), findById(), findAll(), delete() などの
+	//基本的な CRUD 操作を自動的に利用できる
+	List<Item> findByCategoryId(Integer categoryId);
 }
